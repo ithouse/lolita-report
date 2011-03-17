7a4e348b222ebcf8d8f7ef4fdda9fb1b30a47189
Artjoms <tangens88@gmail.com>
2011-03-17 10:56:38 +0200

    izdzezsti lolitas faili

--- lib/lolita-report.rb
+++ lib/lolita-report.rb
@@ -2,5 +2,6 @@ module Lolita
   module Configuration
   autoload(:Reports,"lolita-report/configuration/reports")
   autoload(:Report,"lolita-report/configuration/report")
+  require File.expand_path('../../../lolita/lib/lolita',__FILE__)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,13 +6,6 @@ require File.dirname(__FILE__) + "/factories/factories"
 LOLITA_ORM=:mongoid
 require "orm/#{LOLITA_ORM}"
 require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report/dbi/base')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report/adapter/abstract_adapter')
-#require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report/adapter/active_record')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report/adapter/mongoid')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report/builder')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report/observed_array')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/lolita-report/ruby_ext/accessors')
 
 RSpec.configure do |config|
   # config.mock_with :mocha
